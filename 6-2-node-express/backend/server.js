6e87de5fb9307e0a8133aa7ed1a58d3ac9632c37
FaisalALharbi141 <fash.a.1818@gmail.com>
2025-11-10 16:24:01 +0300

    finished todo 1

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,7 +2,14 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-{/*write code for cors*/}
+const app = express();
+const PORT = 3000;
+
+{/*write code for cors*/
+    app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+}
 
 
 {/*write code to define routes*/}
